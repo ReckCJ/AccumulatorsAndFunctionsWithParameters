8dee0eb17ebd777e3f1a50123e2d4b27e8a787f1
reckcj <reckcj@rose-hulman.edu>
2017-12-03 23:25:25 -0500

    Committing after m7

--- src/m7_why_parameters_are_powerful.py
+++ src/m7_why_parameters_are_powerful.py
@@ -115,7 +115,7 @@ def run_test_better_draw_circles():
 
 
 # ----------------------------------------------------------------------
-# DONE: 3.
+# DONE : 3.
 #   In the previous exercise, you made a MORE POWERFUL version
 #   of draw_circles by introducing a PARAMETER for the amount by
 #   which the radii of the concentric circles increase.
